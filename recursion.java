b68581a2608617fc581d35b07e4066a2b51f2a61
Derek Lao <dlao00@stuy.edu>
2019-02-04 07:07:48 -0500

    sqrt throws overflow

---
+++ recursion.java
@@ -0,0 +1,137 @@
+// import java.lang.Math;
+import java.util.ArrayList;
+
+public class recursion{
+  /*You may write additional private methods */
+
+  /*Recursively find the sqrt using Newton's approximation
+   *tolerance is the allowed percent error the squared answer is away from n.
+   *precondition: n is non-negative
+
+  */
+  public static double sqrt(double n, double tolerance){
+    double guess=n;
+    if(Math.abs((guess-n)/guess) <= tolerance/100)
+    {
+      return guess;
+    }
+    else
+    {
+      return sqrt((n/guess + guess)/2, tolerance/100);
+    }
+  }
+
+  /*Recursively find the n'th fibbonaci number in linear time
+   *fib(0) = 0
+   *fib(1) = 1
+   *fib(5) = 5
+   *precondition: n is non-negative
+   */
+  public static int fib(int n){
+    return fibHelper(n,0,0,1,0);
+  }
+
+  public static int fibHelper(int n, int currentNum,int fibFirst,int fibSecond,int count){
+    if(n<=0) return 0;
+    if(n==1) return 1;
+    if(n>=2)
+    {
+      if(count>=n)
+      {
+        return currentNum;
+      }
+      if(count==0) return fibHelper(0,currentNum,0,1,count+1);
+      if(count==1) return fibHelper(1,currentNum,0,1,count+1);
+      else
+      {
+        fibFirst=fibSecond;
+        fibSecond=currentNum;
+        return fibHelper(n,currentNum+fibFirst+fibSecond,fibSecond,currentNum,count+1);
+      }
+    }
+    return currentNum;
+  }
+
+  /*As Per classwork*/
+  public static ArrayList<Integer> makeAllSums(int n){
+    return makeSums(n,0);
+  }
+
+  public static ArrayList<Integer> makeSums(int n,int sum){
+    ArrayList<Integer> holder=new ArrayList<Integer>();
+    if(n<=1)
+    {
+      holder.add(sum);
+      holder.add(sum+1);
+    }
+    else
+    {
+      holder.add(sum+n);
+      makeSums(n-1,sum+n);
+      holder.add(sum);
+      makeSums(n-1,sum);
+    }
+    return holder;
+  }
+
+  public static void main(String[] args) {
+          System.out.println("sqrt()");
+          System.out.println("-----------------------\n");
+
+          System.out.println("sqrt(4, 0.00001)");
+          System.out.println("EXPECTED: 2");
+          System.out.println(sqrt(4, 0.00001));         // 2
+
+          System.out.println("----");
+
+          System.out.println("sqrt(8, 0.00001)");
+          System.out.println("EXPECTED: ~2.8284");
+          System.out.println(sqrt(8, 0.00001));         // ~2.8284
+
+          System.out.println("----");
+
+          System.out.println("sqrt(0, 0.00001)");
+          System.out.println("EXPECTED: 0");
+          System.out.println(sqrt(0, 0.00001));         // 0
+
+          System.out.println("\n");
+
+          System.out.println("fib()");
+          System.out.println("-----------------------\n");
+
+          System.out.println("fib(0)");
+          System.out.println("EXPECTED: 0");
+          System.out.println(fib(0));                   // 0
+
+          System.out.println("----");
+
+          System.out.println("fib(1)");
+          System.out.println("EXPECTED: 1");
+          System.out.println(fib(1));                   // 1
+
+          System.out.println("----");
+
+          System.out.println("fib(7)");
+          System.out.println("EXPECTED: 13");
+          System.out.println(fib(7));                   // 13
+
+          System.out.println("makeAllSums()");
+          System.out.println("-----------------------\n");
+
+          System.out.println("makeAllSums(0)");
+          System.out.println("EXPECTED: [0]");
+          System.out.println(makeAllSums(0));           // [0]
+
+          System.out.println("----");
+
+          System.out.println("makeAllSums(1)");
+          System.out.println("EXPECTED: [1, 0]");
+          System.out.println(makeAllSums(1));           // [1, 0]
+
+          System.out.println("----");
+
+          System.out.println("makeAllSums(3)");
+          System.out.println("EXPECTED: [0, 3, 2, 5, 1, 4, 3, 6]");
+          System.out.println(makeAllSums(3));           // [0, 3, 2, 5, 1, 4, 3, 6]
+        }
+}
